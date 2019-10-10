49e980297617536bd3e7ba2528203454d39f2283
iwamoto <iwamoto@cshool.jp>
2019-10-10 12:27:39 +0900

    if, 標準入力を修了

--- 01.basic/main.cpp
+++ 01.basic/main.cpp
@@ -2,6 +2,12 @@ int default_plus(int x, int y) {
     return x + y;
 }
 
+// ロケットが発射可能かどうかを返す関数
+bool is_rocket_ready_to_launch() {
+    // まだだよ
+    return false;
+}
+
 int main() {
     std::cout << "one"s;
     std::cout << "two"s;
@@ -116,20 +122,95 @@ int main() {
     std::cout << default_plus(1, 2) << "\n"s;
 
 
-    auto x = 0 ;
+    auto x = 0;
     {
-        auto x = 1 ;
+        auto x = 1;
         {
-            auto x = 2 ;
+            auto x = 2;
             // 2
-            std::cout << x ;
+            std::cout << x;
         }
         // 1
-        std::cout << x ;
-        x = 42 ;
+        std::cout << x;
+        x = 42;
         // 42
-        std::cout << x ;
+        std::cout << x;
     }
     // 0
-    std::cout << x ;
+    std::cout << x;
+
+    bool correct = true;
+    bool wrong = false;
+
+    // std::boolalphaを一度出力すると、それ移行の値が、true、falseで出るようになる.
+    //　std::noboolalpha でもとに戻る.
+    std::cout << std::boolalpha;
+    std::cout << true << false;
+    std::cout << std::noboolalpha;
+    std::cout << true << false;
+
+    // ロケットが発射可能ではないときに実行される
+    if (!is_rocket_ready_to_launch()) {   // もうしばらくそのままでお待ちください
+        std::cout << "Standby...\n";
+    }
+
+    auto boolA = []() {
+        std::cout << "a\n"s;
+        return false;
+    };
+    auto boolB = []() {
+        std::cout << "b\n"s;
+        return true;
+    };
+
+    // 短絡評価. aがfalseなのでbは実行されない.
+    bool boolC = boolA() && boolB();
+    std::cout << std::boolalpha << boolC << std::noboolalpha;
+
+    auto xx = 1;
+
+    // 代入式を()で囲むことで、意図的に代入を行ってると明示できる.
+    if ((xx = 0))
+        std::cout << "x is 123.\n"s;
+    else
+        std::cout << "x is NOT 123.\n"s;
+
+    auto calcBmi = [](double height, double mass) {
+        return mass / (height * height);
+    };
+
+    auto getBmiStatus = [](double bmi) {
+        if (bmi < 18.5)
+            return "Underweight.\n"s;
+        else if (bmi < 25.0)
+            return "Normal.\n"s;
+        else if (bmi < 30.0)
+            return "Overweight.\n"s;
+        else
+            return "Obese."s;
+    };
+
+    // 身長の入力
+    double ownHeight{};
+    std::cout << "height(m)>";
+    std::cin >> height;
+
+    // 体重の入力
+    double ownMass{};
+    std::cout << "mass(kg)>";
+    std::cin >> mass;
+
+    auto ownBmi = calcBmi(ownHeight, ownMass);
+
+    std::cout << "BMI = " << ownBmi << "\n";
+    std::cout << "Your Bmi is " << getBmiStatus(ownBmi) << "\n";
+
+    // boolの標準入力は通常0 || not 0で行う
+    //bool b{} ;
+    //std::cin >> b ;
+
+    // true, falseで行いたい場合、std::cinにstd::boolalphaを入力させる.
+    //bool b{} ;
+    //std::cin >> std::boolalpha >> b ;
+    //std::cout << std::boolalpha << b ;
 }
